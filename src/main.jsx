79a0c48a7da70dd49f7abdd5318cfd0620e48e73
Aurélien <aurelienarnaud921@gmail.com>
2025-02-07 12:06:21 +0700

    feature: creation des fichier scss

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
+// Import React
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// Import Components
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
+// Import Pages
 import Home from './pages/home/home';
 import Logement from './pages/logement/logement';
 import Apropos from './pages/a_propos/a_propos';
 import Erreur from './pages/erreur/erreur';
+// Import Style
+import './style/style.scss';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
