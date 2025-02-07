2fa2329e076ef53761fe39a7e40a84867405e8e0
Aurélien <aurelienarnaud921@gmail.com>
2025-02-07 13:50:33 +0700

    feature : creation du header - importation du logo - creation du component - creation du scss

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,27 @@
 import { Link } from 'react-router-dom';
+import Casa_logo_red from '../../assets/logo/logo_casa_red.png';
 
 function Header() {
   return (
-    <>
-      <Link to="/">acceuil</Link>
-      <Link to="/a_propos">a propos</Link>
-      <p>header</p>
-    </>
+    <header>
+      <img src={Casa_logo_red} alt="Logo kasa" className="logo-header" />
+
+      <nav>
+        <ul className="list-nav-header">
+          <li>
+            <Link to="/" className="nav-link">
+              Accueil
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/a_propos" className="nav-link">
+              A Propos
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
