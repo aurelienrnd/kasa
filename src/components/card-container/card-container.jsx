4586a36110183360a785f933620c89dd0667a904
Aurélien <aurelienarnaud921@gmail.com>
2025-02-14 09:17:18 +0700

    creation du hook getFeetch

--- src/components/card-container/card-container.jsx
+++ src/components/card-container/card-container.jsx
@@ -1,47 +1,13 @@
-import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import UseGetFetch from '../../utile/use-hook/use-getfetch';
 
 /** Affiche les cartes hebergements sur la page d'aceuille.
  * @function {fetchData} une requette api pour recupere les difèrent logement.
  * @returns {JSX.Element} une div avec toute les carte hebergements.
  */
 function CardContainer() {
-  const [data, setData] = useState([]); // Stocker les données récupérées
-  const [loading, setLoading] = useState(true); // Gérer l'état de chargement
-  const [error, setError] = useState(''); // Gérer les erreurs
-
-  // fonction get
-  const fetchData = async () => {
-    try {
-      const response = await fetch('/data/accommodation-data.json');
-
-      // Pour tester le lorder et simuler un chargement plus lent
-      await new Promise((resolve) => setTimeout(resolve, 1000)); // Ajoute un délai de 3s
-
-      if (response.ok) {
-        /*ci il ne trouve pas le fichier dans public vite retourne index.htm avec une reponsse 200
-        - solution: tester ci responce contien un fichier .json, ci non, c'est que vite retourne index.html*/
-        const contentType = response.headers.get('content-type');
-        if (!contentType.includes('application/json')) {
-          throw new Error("Le serveur a retourné un fichier qui n'est pas du JSON !");
-        }
-        const result = await response.json();
-        console.log(result);
-        setData(result); // Mise à jour des données
-      } else {
-        throw new Error('Erreur lors du chargement des données');
-      }
-    } catch (err) {
-      console.log(err.message);
-      setError(err.message);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const url = '/data/accommodation-data.json'; // Chemin vers ton fichier JSON ou API
+  const { loading, data, error } = UseGetFetch(url);
 
   // affichage d'un message de chargement et d'erreur
   const loaderCircle = <div className="loader-circle"></div>;
--- src/utile/use-hook/use-getfetch.jsx
+++ src/utile/use-hook/use-getfetch.jsx
@@ -1,3 +1,45 @@
 import { useState, useEffect } from 'react';
 
-function UseGetFetch() {}
+function UseGetFetch(url) {
+  const [data, setData] = useState([]); // Stocker les données récupérées
+  const [loading, setLoading] = useState(true); // Gérer l'état de chargement
+  const [error, setError] = useState(''); // Gérer les erreurs
+
+  useEffect(() => {
+    if (!url) return;
+    setLoading(true);
+    async function fetchData() {
+      try {
+        const response = await fetch(url);
+
+        // Pour tester le lorder et simuler un chargement plus lent
+        await new Promise((resolve) => setTimeout(resolve, 1000)); // Ajoute un délai de 3s
+
+        if (response.ok) {
+          /*ci il ne trouve pas le fichier dans public vite retourne index.htm avec une reponsse 200
+            - solution: tester ci responce contien un fichier .json, ci non, c'est que vite retourne index.html*/
+          const contentType = response.headers.get('content-type');
+          if (!contentType.includes('application/json')) {
+            throw new Error("Le serveur a retourné un fichier qui n'est pas du JSON !");
+          }
+
+          const result = await response.json();
+          console.log(result);
+          setData(result); // Mise à jour des données
+        } else {
+          throw new Error('Erreur lors du chargement des données');
+        }
+      } catch (err) {
+        console.log(err);
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, [url]);
+
+  return { loading, data, error };
+}
+
+export default UseGetFetch;
